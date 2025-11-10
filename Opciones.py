fb9444c4907167fc5db981202190a6b40684a7c7
GonzaloGarridoFas <gonzalogarrido@mi.unc.edu.ar>
2025-11-10 10:55:00 -0300

    Cambios finales
    Se borraron leyendas de consignas, se agrego un limitador de tiempo y correccion de la opcion 6

--- Opciones.py
+++ Opciones.py
@@ -1,28 +1,14 @@
-"""
-#• Agregar un país con todos los datos necesarios para almacenarse (No se permiten campos vacios).
-#• Actualizar los datos de Población y Superfice de un Pais.
-#• Buscar un país por nombre (coincidencia parcial o exacta).
-#• Filtrar países por:
-# o Continente
-# o Rango de población
-# o Rango de superficie
-#• Ordenar países por:
-# o Nombre
-# o Población
-# o Superficie (ascendente o descendente)
-#• Mostrar estadísticas:
-# o País con mayor y menor población
-# o Promedio de población
-# o Promedio de superficie
-# o Cantidad de países por continente
-"""
 import csv
 from tp_integrador_1 import ingresar_pais, actualizar_datos, buscar_pais, filtrar_paises, ordenar_paises, existe_pais, estadisticas, obtener_paises, validador, seleccionar_continente, primera_mayuscula
-resultado = None
 opcion = -1
 pais = ""
+intento = 1
 
 while opcion != 0:
+      
+    if intento >3:
+        print("Has superado el numero de intentos, el programa se cerrara.")
+        break
 
     print("Menú de opciones:")
     print("1. Agregar país")
@@ -30,25 +16,29 @@ while opcion != 0:
     print("3. Buscar país por nombre")
     print("4. Filtras países")
     print("5. Ordenar países")
-    print("6. Mostrar caracteristicas")
+    print("6. Mostrar estadisticas")
     print("0. Salir")
 
     opcion = (input("Seleccione una opción del 1 al 6, si quiere salir seleccion '0': "))
 
     if not opcion.isdigit():
         print("No es una opcion valida, por favor ingresa una opcion del 1 al 6, o 0 si queres salir.")
+        intento = intento + 1
         continue
 
     opcion = int(opcion)
 
     if opcion < 0 or opcion > 6:
         print("No es una opcion valida, por favor ingresa una opcion del 1 al 6, o 0 si queres salir.")
+        intento = intento + 1
         continue
+    
+  
 
     if opcion == 1:
         nombre = input("Ingrese el nombre del país: ").lower()
-        cantidad_habitantes = validador(int(input("ingrese la cantidad de habitantes: ")))
-        superficie = validador(int(input("Ingrese la superficie del país expresada en metros cuadrados (expresado unicamente en numeros y sin caracteres especiales: ")))
+        cantidad_habitantes = validador(int(input("ingrese la cantidad de habitantes (Expresado en numeros y sin ',' o '.'): ")))
+        superficie = validador(int(input("Ingrese la superficie del país expresada en metros cuadrados (expresado unicamente en numeros y sin caracteres especiales): ")))
         continente = seleccionar_continente()
         pais = {"nombre": primera_mayuscula(nombre), "poblacion": cantidad_habitantes, "superficie": superficie, "continente": continente}
         ingresar_pais(pais)
@@ -66,7 +56,7 @@ while opcion != 0:
       ordenar_paises()
 
     elif opcion == 6:
-      pass
+      estadisticas()
 
     elif opcion == 0:
       print("Muchas gracias, los esperamos de vuelta")
